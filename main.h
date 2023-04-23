365d53e87b6f078c417a235035b2594d72ceada9
Noransaber <noransaber685@gmail.com>
2023-04-23 13:16:47 -0700

    utils

--- main.h
+++ main.h
@@ -106,7 +106,7 @@ int write_unsgnd(int is_negative, int ind,
 char buffer[],
 	int flags, int width, int precision, int size);
 
-/****************** UTILS ******************/
+/***** UTILS ***/
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
