06a093ad436d38d703d27d782eca9aa0b29006a6
wadiiAnr <wadii.anr@gmail.com>
2023-04-23 13:06:19 -0700

    update handlePrint

--- handle_print.c
+++ handle_print.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
- * handle_print - Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
- * @ind: ind.
- * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
- * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
- * Return: 1 or 2;
- */
+*handle_print - Prints an argument based on its type
+*@fmt: Formatted string in which to print the arguments.
+*@list: List of arguments to be printed.
+*@ind: ind.
+*@buffer: Buffer array to handle print.
+*@flags: Calculates active flags
+*@width: get width.
+*@precision: Precision specification
+*@size: Size specifier
+*Return: 1 or 2;
+*/
+
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
@@ -26,25 +27,25 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		if (fmt[*ind] == fmt_types[i].fmt)
 			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
-	if (fmt_types[i].fmt == '\0')
+if (fmt_types[i].fmt == '\0')
+{
+	if (fmt[*ind] == '\0')
+	return (-1);
+	unknow_len += write(1, "%%", 1);
+	if (fmt[*ind - 1] == ' ')
+		unknow_len += write(1, " ", 1);
+	else if (width)
 	{
-		if (fmt[*ind] == '\0')
-			return (-1);
-		unknow_len += write(1, "%%", 1);
-		if (fmt[*ind - 1] == ' ')
-			unknow_len += write(1, " ", 1);
-		else if (width)
-		{
+		--(*ind);
+		while (fmt[*ind] != ' ' && fmt[*ind] != '%')
 			--(*ind);
-			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
-				--(*ind);
-			if (fmt[*ind] == ' ')
-				--(*ind);
-			return (1);
-		}
-		unknow_len += write(1, &fmt[*ind], 1);
-		return (unknow_len);
+		if (fmt[*ind] == ' ')
+			--(*ind);
+		return (1);
 	}
+	unknow_len += write(1, &fmt[*ind], 1);
+	return (unknow_len);
+}
 	return (printed_chars);
 }
 
